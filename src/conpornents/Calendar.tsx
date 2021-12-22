4f63907ff52762943cafaf413584084c8025d2b5
Atsuko Yamada <60997895+AtsukoYYY@users.noreply.github.com>
2021-12-22 17:34:22 +0900

    initial commit

--- src/conpornents/Calendar.tsx
+++ src/conpornents/Calendar.tsx
@@ -2,22 +2,41 @@ import Box from "@mui/material/Box";
 import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
 import FullCalendar from "@fullcalendar/react";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
 import listPlugin from "@fullcalendar/list";
 
 export const Calendar = () => {
+  const events = [
+    {
+      id: 1,
+      title: "録音室A 　使用者：〇〇　××　△△",
+      start: "2021-12-22T09:00:00"
+    },
+    {
+      title: "録音室B 　使用者：〇〇　××",
+      start: "2021-12-07T09:00:00"
+    },
+    {
+      title: "録音室E 　使用者：〇〇　××",
+      start: "2021-12-07T09:00:00"
+    },
+    {
+      title: "録音室E 　使用者：〇〇　××",
+      start: "2021-12-07T13:00:00"
+    }
+  ];
   const fabStyle = {
     bottom: 16,
     right: 16,
-
     position: "fixed"
   };
   return (
     <Box sx={{ mb: 2, flexGrow: 1 }}>
       <FullCalendar
-        locale="ja" // ロケール設定。
-        plugins={[timeGridPlugin, dayGridPlugin, listPlugin]} // 週表示、月表示、日付等のクリックを可能にするプラグインを設定。
+        locale="ja"
+        editable={true}
+        droppable={true}
+        plugins={[interactionPlugin, listPlugin]} // 週表示、月表示、日付等のクリックを可能にするプラグインを設定。
         initialView="listMonth"
         titleFormat={{
           year: "numeric",
@@ -28,32 +47,7 @@ export const Calendar = () => {
           center: "title",
           end: "prev,today,next"
         }}
-        events={[
-          {
-            title: "録音室A",
-            start: "2021-12-22T09:00:00",
-            end: "2021-12-07T22:00:00",
-            display: "block"
-          },
-          {
-            title: "録音室B",
-            start: "2021-12-07T09:00:00",
-            end: "2021-12-07T12:00:00",
-            display: "block"
-          },
-          {
-            title: "録音室E",
-            start: "2021-12-07T09:00:00",
-            end: "2021-12-07T12:00:00",
-            display: "block"
-          },
-          {
-            title: "録音室E",
-            start: "2021-12-07T13:00:00",
-            end: "2021-12-07T17:00:00",
-            display: "block"
-          }
-        ]}
+        events={events}
       />
       <Fab sx={fabStyle} color="primary" aria-label="add">
         <AddIcon />
